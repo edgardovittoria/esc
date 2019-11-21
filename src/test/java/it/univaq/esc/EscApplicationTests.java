dcddc6e22bc51f4a22667fcfb7553fe4d198a425
edgardovittoria <edgardovittoria@hotmail.it>
2019-11-21 15:08:58 +0100

    creazione progetto

---
+++ src/test/java/it/univaq/esc/EscApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univaq.esc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
