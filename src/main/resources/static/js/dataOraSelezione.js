48d5608f7179b41faab0202c6ca3db018346d114
Draco112358 <gianni.frittella@gmail.com>
2021-03-29 10:09:40 +0200

    aggiornamento 2

--- src/main/resources/static/js/dataOraSelezione.js
+++ src/main/resources/static/js/dataOraSelezione.js
@@ -1,47 +1,55 @@
-$(".timepicker").timepicker({
-  timeFormat: "HH:mm",
-  interval: 30,
-  minTime: "10:00",
-  maxTime: "21:00",
-  defaultTime: "10",
-  startTime: "10:00",
-  dynamic: false,
-  dropdown: true,
-  scrollbar: true,
-  change: function () {
-    $(".timepickerFine").remove();
-    $("#inputFine").append(
-      '<input id="timepickerFine" class="timepickerFine" />'
-    );
-    $(".timepickerFine").timepicker({
-      timeFormat: "HH:mm",
-      minTime: $(".timepicker").val(),
-      maxTime: "22:00",
-      defaultTime: $(".timepicker").val(),
-      startTime: $(".timepicker").val(),
-      dynamic: false,
-      dropdown: true,
-      scrollbar: true,
-      change: function(){
-        var dataOraInizio = $("#start").val()+"T"+$(".timepicker").val();
-        var dataOraFine = $("#start").val()+"T"+$(".timepickerFine").val();
-        aggiornaImpiantiDisponibili(dataOraInizio, dataOraFine);
-      }
-    });
-    
-  },
-});
+   $(".timepicker").timepicker({
+    timeFormat: "HH:mm",
+    interval: 30,
+    minTime: "10:00",
+    maxTime: "21:00",
+    defaultTime: "10",
+    startTime: "10:00",
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true,
+    change: function () {
+      $(".timepickerFine").remove();
+      $("#inputFine").append(
+        '<input id="timepickerFine" class="timepickerFine" />'
+      );
+      $(".timepickerFine").timepicker({
+        timeFormat: "HH:mm",
+        minTime: $(".timepicker").val(),
+        maxTime: "22:00",
+        defaultTime: $(".timepicker").val(),
+        startTime: $(".timepicker").val(),
+        dynamic: false,
+        dropdown: true,
+        scrollbar: true,
+        change: function(){
+          var dataOraInizio = $("#start").val()+"T"+$(".timepicker").val();
+          var dataOraFine = $("#start").val()+"T"+$(".timepickerFine").val();
+          var sportSelezionato = $("input[name='sportPraticabile']:checked").val();
+          aggiornaImpiantiDisponibili(dataOraInizio, dataOraFine, sportSelezionato);
+        }
+      });
+      
+    },
+  });
 
-function aggiornaImpiantiDisponibili(dataOraInizio, dataOraFine) {
+
+
+function aggiornaImpiantiDisponibili(dataOraInizio, dataOraFine, sportSelezionato) {
   var appuntamento = [dataOraInizio, dataOraFine];
-  console.log(JSON.stringify(appuntamento));
+  var dati = {
+    'orario': appuntamento,
+    'sport' : sportSelezionato
+  };
+  console.log(dati);
   $.ajax({
     method: "POST",
     url: "http://localhost:8080/aggiornaOpzioni/impianti",
     contentType: "application/json; charset=UTF-8",
-    data: JSON.stringify(appuntamento),
-    statusCode: {
-      200: function (impianti) {
+    dataType: "json",
+    data: JSON.stringify(dati),
+    success: 
+       function (impianti) {
         console.log(impianti);
         $('#selezioneImpianto').html('');
         var options = '';    
@@ -50,7 +58,6 @@ function aggiornaImpiantiDisponibili(dataOraInizio, dataOraFine) {
         }) ;        
         $('#selezioneImpianto').append(options); 
       },
-    },
   });
 }
 
