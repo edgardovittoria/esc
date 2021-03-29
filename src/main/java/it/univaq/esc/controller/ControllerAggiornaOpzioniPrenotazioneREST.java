6f95d1b62186c10773bf199840e5a7566edfe0ef
Draco112358 <gianni.frittella@gmail.com>
2021-03-29 10:36:20 +0200

    aggiornamento template

--- src/main/java/it/univaq/esc/controller/ControllerAggiornaOpzioniPrenotazioneREST.java
+++ src/main/java/it/univaq/esc/controller/ControllerAggiornaOpzioniPrenotazioneREST.java
@@ -1,6 +1,7 @@
 package it.univaq.esc.controller;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,12 +28,14 @@ public class ControllerAggiornaOpzioniPrenotazioneREST {
     private EffettuaPrenotazioneHandler controller;
 
     @PostMapping("/impianti")
-    public @ResponseBody HashMap<Integer, String> getListaImpianti(@RequestParam("dati['sport']") String sport, @RequestParam("orario") List<LocalDateTime> appuntamento){        
-        List<Impianto> impiantiDisponibili =  controller.getImpiantiDisponibiliByOrario(appuntamento.get(0), appuntamento.get(1));
+    public @ResponseBody HashMap<Integer, String> getListaImpianti(@RequestBody HashMap<String, Object> dati){        
+
+        List<String> orario = (ArrayList<String>)dati.get("orario");
+        List<Impianto> impiantiDisponibili =  controller.getImpiantiDisponibiliByOrario(LocalDateTime.parse(orario.get(0)), LocalDateTime.parse(orario.get(1)));
         HashMap<Integer, String> datiImpiantiDisponibili = new HashMap<Integer, String>();
         for(Impianto impianto : impiantiDisponibili){
             for(ImpiantoSpecs specifica : impianto.getSpecificheImpianto()){
-                if(specifica.getSportPraticabile().getNome().equals(sport)){
+                if(specifica.getSportPraticabile().getNome().equals(dati.get("sport"))){
                     datiImpiantiDisponibili.put(impianto.getIdImpianto(), impianto.getSpecificheImpianto().get(0).getTipoPavimentazione().toString());
                 }
             }
