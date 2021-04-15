54a03170d6fe628f7df28b953d36a3a7132f6d68
Draco112358 <gianni.frittella@gmail.com>
2021-04-15 10:19:34 +0200

    aggiornamento metodo controller prenotazione rest

--- src/main/java/it/univaq/esc/controller/EffettuaPrenotazioneHandlerRest.java
+++ src/main/java/it/univaq/esc/controller/EffettuaPrenotazioneHandlerRest.java
@@ -1,26 +1,35 @@
 package it.univaq.esc.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.univaq.esc.dtoObjects.ImpiantoDTO;
 import it.univaq.esc.dtoObjects.SportDTO;
 import it.univaq.esc.dtoObjects.SportivoDTO;
+import it.univaq.esc.factory.FactorySpecifichePrenotazione;
+import it.univaq.esc.model.Appuntamento;
 import it.univaq.esc.model.Calendario;
 import it.univaq.esc.model.Impianto;
 import it.univaq.esc.model.ImpiantoSpecs;
+import it.univaq.esc.model.Prenotazione;
+import it.univaq.esc.model.PrenotazioneSpecs;
 import it.univaq.esc.model.RegistroImpianti;
+import it.univaq.esc.model.RegistroPrenotazioni;
 import it.univaq.esc.model.RegistroSportivi;
 import it.univaq.esc.model.Sport;
+import it.univaq.esc.model.Sportivo;
 import it.univaq.esc.model.UtentePolisportivaAbstract;
 
 @RestController
@@ -32,13 +41,18 @@ public class EffettuaPrenotazioneHandlerRest {
 
     @Autowired
     private RegistroSportivi registroSportivi;
+
+    @Autowired
+    private RegistroPrenotazioni registroPrenotazioni;
+
+    private Prenotazione prenotazioneInAtto;
     
     public EffettuaPrenotazioneHandlerRest(){}
 
 
-    @GetMapping("/sportPraticabili")
-    @CrossOrigin
-    public @ResponseBody List<SportDTO> getSportPraticabiliPolisportiva(){
+    // @GetMapping("/sportPraticabili")
+    // @CrossOrigin
+    private List<SportDTO> getSportPraticabiliPolisportiva(){
         List<Sport> listaSportPraticabili = new ArrayList<Sport>();
         Set<Sport> setSportPraticabili = new HashSet<Sport>();
         for (Impianto impianto : registroImpianti.getListaImpiantiPolisportiva()) {
@@ -57,9 +71,9 @@ public class EffettuaPrenotazioneHandlerRest {
     }
 
 
-    @GetMapping("/impiantiDisponibili")
-    @CrossOrigin
-    public @ResponseBody List<ImpiantoDTO> getImpiantiDisponibili(){
+    // @GetMapping("/impiantiDisponibili")
+    // @CrossOrigin
+    private List<ImpiantoDTO> getImpiantiDisponibili(){
         Calendario calendario = new Calendario();
         List<Impianto> listaImpiantiDisponibili = new ArrayList<Impianto>();        
         for(Impianto impianto : registroImpianti.getListaImpiantiPolisportiva()){
@@ -77,9 +91,9 @@ public class EffettuaPrenotazioneHandlerRest {
         
     }
 
-    @GetMapping("/sportiviPolisportiva")
-    @CrossOrigin
-    public @ResponseBody List<SportivoDTO> getSportiviPolisportiva(){
+    // @GetMapping("/sportiviPolisportiva")
+    // @CrossOrigin
+    private List<SportivoDTO> getSportiviPolisportiva(){
         List<SportivoDTO> listaSportiviDTO = new ArrayList<SportivoDTO>();
         for(UtentePolisportivaAbstract utente : this.registroSportivi.getListaSportivi()){
             SportivoDTO sportivoDTO = new SportivoDTO();
@@ -89,4 +103,47 @@ public class EffettuaPrenotazioneHandlerRest {
 
         return listaSportiviDTO;
     }
-}
+
+        @GetMapping("/avviaNuovaPrenotazione")
+        @CrossOrigin
+        public @ResponseBody Map<String, Object> avviaNuovaPrenotazioneImpianto(@RequestParam(name="email") String emailSportivoPrenotante, @RequestParam(name="tipoPrenotazione") String tipoPrenotazione){
+            UtentePolisportivaAbstract sportivoPrenotante = this.getRegistroSportivi().getSportivoDaEmail(emailSportivoPrenotante);
+            int lastIdPrenotazione = this.registroPrenotazioni.getLastIdPrenotazione();
+            PrenotazioneSpecs prenotazioneSpecs = FactorySpecifichePrenotazione.getSpecifichePrenotazione(tipoPrenotazione);
+            setPrenotazioneInAtto(new Prenotazione(lastIdPrenotazione, prenotazioneSpecs));
+            prenotazioneSpecs.setPrenotazioneAssociata(getPrenotazioneInAtto());
+            getPrenotazioneInAtto().setSportivoPrenotante(sportivoPrenotante);
+            Appuntamento appuntamento = new Appuntamento();
+            appuntamento.setPrenotazioneSpecsAppuntamento(prenotazioneSpecs);
+            appuntamento.aggiungiPartecipante(sportivoPrenotante);
+           
+            Map<String, Object> mappaValori = new HashMap<String, Object>();
+            mappaValori.put("sportPraticabili", this.getSportPraticabiliPolisportiva());
+            mappaValori.put("impiantiDisponibili", this.getImpiantiDisponibili());
+            mappaValori.put("sportiviPolisportiva", this.getSportiviPolisportiva());
+
+            return mappaValori;
+        }
+
+
+        private RegistroSportivi getRegistroSportivi(){
+            return this.registroSportivi;
+        }
+
+        private RegistroImpianti getRegistroImpianti(){
+            return this.registroImpianti;
+        }
+
+        private RegistroPrenotazioni getRegistroPrenotazioni(){
+            return this.registroPrenotazioni;
+        }
+
+        private Prenotazione getPrenotazioneInAtto(){
+            return this.prenotazioneInAtto;
+        }
+
+        private void setPrenotazioneInAtto(Prenotazione prenotazione){
+            this.prenotazioneInAtto = prenotazione;
+        }
+    }
+
