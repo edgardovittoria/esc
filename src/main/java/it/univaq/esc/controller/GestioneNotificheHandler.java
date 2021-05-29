c8e73f127b528a3ac35d5dd78b293147119c1e68
Draco112358 <gianni.frittella@gmail.com>
2021-05-29 12:17:35 +0200

    aggiornato metodo getDettagliNotifiche del controller GestioneNotifiche

--- src/main/java/it/univaq/esc/controller/GestioneNotificheHandler.java
+++ src/main/java/it/univaq/esc/controller/GestioneNotificheHandler.java
@@ -1,5 +1,9 @@
 package it.univaq.esc.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.hibernate.loader.plan.exec.process.spi.ReturnReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,15 +13,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import it.univaq.esc.dtoObjects.NotificabileDTO;
 import it.univaq.esc.dtoObjects.PrenotazioneDTO;
-
+import it.univaq.esc.model.prenotazioni.Appuntamento;
 import it.univaq.esc.model.prenotazioni.Prenotazione;
+import it.univaq.esc.model.prenotazioni.RegistroAppuntamenti;
 import it.univaq.esc.model.prenotazioni.RegistroPrenotazioni;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 @RestController
 @RequestMapping("/notifiche")
@@ -28,13 +35,20 @@ public class GestioneNotificheHandler {
 	@Autowired
 	private RegistroPrenotazioni RegistroPrenotazioni;
 	
+	@Autowired
+	private RegistroAppuntamenti RegistroAppuntamenti;
+	
 	@GetMapping("/dettagliNotifica")
 	@CrossOrigin
 	public @ResponseBody NotificabileDTO getDettagliNotifica(@RequestParam(name = "idEvento") Long idEvento) {
 		Prenotazione prenotazione = getRegistroPrenotazioni().getPrenotazioneById(idEvento);
-		
+ 
 		PrenotazioneDTO prenDTO = new PrenotazioneDTO();
-		prenDTO.impostaValoriDTO(prenDTO);
+		Map<String, Object> mappaPrenotazione = new HashMap<String, Object>();
+		mappaPrenotazione.put("prenotazione", prenotazione);
+		List<Appuntamento> appuntamentiPrenotazione = getRegistroAppuntamenti().getAppuntamentiByPrenotazioneId(idEvento);
+		mappaPrenotazione.put("appuntamentiPrenotazione", appuntamentiPrenotazione);
+		prenDTO.impostaValoriDTO(mappaPrenotazione);
 		
 		return prenDTO;
 	}
