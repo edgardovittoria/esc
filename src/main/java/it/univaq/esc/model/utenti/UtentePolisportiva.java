453b4c25c865c7ee11c959d45d589a996d4eabd7
Draco112358 <gianni.frittella@gmail.com>
2021-06-23 16:31:38 +0200

    speriamo che funziona

--- src/main/java/it/univaq/esc/model/utenti/UtentePolisportiva.java
+++ src/main/java/it/univaq/esc/model/utenti/UtentePolisportiva.java
@@ -2,88 +2,181 @@ package it.univaq.esc.model.utenti;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
-
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@DiscriminatorValue(value = "UtenteBase")
-@Getter(value = AccessLevel.PRIVATE)
-@Setter(value = AccessLevel.PRIVATE)
-@NoArgsConstructor
-public class UtentePolisportiva extends UtentePolisportivaAbstract implements Serializable {
-	@Column
-	private String nome;
-	@Column
-	private String cognome;
-	@Column
-	private String email;
-	@Column
-	private String password;
+@Getter(value = AccessLevel.PRIVATE) @Setter(value = AccessLevel.PRIVATE)
+public class UtentePolisportiva implements Serializable{
+	
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private Integer idUtente;
+		
+		@OneToOne(cascade = CascadeType.ALL)
+		@JoinColumn
+		private AnagraficaUtente anagraficaUtente;
+		
+		@OneToMany(cascade = CascadeType.ALL)
+		@JoinColumn
+		@LazyCollection(LazyCollectionOption.FALSE)
+		private List<ProfiloUtente> profiliAssociati = new ArrayList<ProfiloUtente>();
 
-	public UtentePolisportiva(String nome, String cognome, String email, String password) {
-		this.setCognome(cognome);
-		this.setEmail(email);
-		this.setNome(nome);
-		this.setPassword(password);
-	}
+		
+		public  UtentePolisportiva(AnagraficaUtente anagraficaUtente) {
+			setAnagraficaUtente(anagraficaUtente);
+		}
 
-	@Override
-	public void setProprieta(Map<String, Object> mappaProprieta) {
-		for (String chiave : mappaProprieta.keySet()) {
-			switch (chiave) {
-			case "nome":
-				this.setNome((String) mappaProprieta.get(chiave));
-				break;
-			case "cognome":
-				this.setCognome((String) mappaProprieta.get(chiave));
-				break;
-			case "email":
-				this.setEmail((String) mappaProprieta.get(chiave));
-				break;
-			case "password":
-				this.setPassword((String) mappaProprieta.get(chiave));
-				break;
-			default:
-				break;
+//		@Override
+//		public void setProprieta(Map<String, Object> mappaProprieta) {
+//			for (String chiave : mappaProprieta.keySet()) {
+//				switch (chiave) {
+//				case "nome":
+//					this.setNome((String) mappaProprieta.get(chiave));
+//					break;
+//				case "cognome":
+//					this.setCognome((String) mappaProprieta.get(chiave));
+//					break;
+//				case "email":
+//					this.setEmail((String) mappaProprieta.get(chiave));
+//					break;
+//				case "password":
+//					this.setPassword((String) mappaProprieta.get(chiave));
+//					break;
+//				default:
+//					break;
+//				}
+//			}
+	//
+//		}
+
+//		@Override
+//		public Map<String, Object> getProprieta() {
+//			Map<String, Object> mappaProprieta = new HashMap<String, Object>();
+//			mappaProprieta.put("nome", this.getNome());
+//			mappaProprieta.put("cognome", this.getCognome());
+//			mappaProprieta.put("email", this.getEmail());
+//			mappaProprieta.put("password", this.getPassword());
+	//
+//			return mappaProprieta;
+//		}
+
+		
+		public List<String> getRuoli() {
+			List<String> listaRuoli = new ArrayList<String>();
+			getProfiliAssociati().forEach((profilo) -> listaRuoli.add(profilo.getRuoloRelativo().toString()));
+			
+			return listaRuoli;
+		}
+
+
+		public boolean isEqual(UtentePolisportiva utenteDaConfrontare) {
+			if (this.getEmail().equals(utenteDaConfrontare.getEmail())) {
+				return true;
+			}
+			return false;
+		}
+		
+		
+		
+		public ProfiloSportivo comeSportivo() {
+			if(is(TipoRuolo.SPORTIVO)) {
+				return (ProfiloSportivo) getProfiloAssociatoRealtivoA(TipoRuolo.SPORTIVO);
+			}
+			return null;
+		}
+		
+		public ProfiloIstruttore comeIstruttore() {
+			if(is(TipoRuolo.ISTRUTTORE)) {
+				return (ProfiloIstruttore) getProfiloAssociatoRealtivoA(TipoRuolo.ISTRUTTORE);
+			}
+			return null;
+		}
+		
+		public ProfiloManutentore comeManutentore() {
+			if(is(TipoRuolo.MANUTENTORE)) {
+				return (ProfiloManutentore) getProfiloAssociatoRealtivoA(TipoRuolo.MANUTENTORE);
+			}
+			return null;
+		}
+		
+		public ProfiloDirettore comeDirettore() {
+			if(is(TipoRuolo.DIRETTORE)) {
+				return (ProfiloDirettore) getProfiloAssociatoRealtivoA(TipoRuolo.DIRETTORE);
+			}
+			return null;
+		}
+		
+		//commento
+		public boolean is(TipoRuolo ruolo) {
+			for(ProfiloUtente profiloUtente : getProfiliAssociati()) {
+				if(profiloUtente.isProfilo(ruolo)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private ProfiloUtente getProfiloAssociatoRealtivoA(TipoRuolo ruolo) {
+			for(ProfiloUtente profiloUtente : getProfiliAssociati()) {
+				if(profiloUtente.isProfilo(ruolo)) {
+					return profiloUtente;
+				}
+			}
+			return null;
+		}
+		
+		public void aggiungi(ProfiloUtente nuovoProfiloUtente) {
+			TipoRuolo ruolo = nuovoProfiloUtente.getRuoloRelativo();
+			if(!is(ruolo)) {
+				getProfiliAssociati().add(nuovoProfiloUtente);
 			}
 		}
-
-	}
-
-	@Override
-	public Map<String, Object> getProprieta() {
-		Map<String, Object> mappaProprieta = new HashMap<String, Object>();
-		mappaProprieta.put("nome", this.getNome());
-		mappaProprieta.put("cognome", this.getCognome());
-		mappaProprieta.put("email", this.getEmail());
-		mappaProprieta.put("password", this.getPassword());
-
-		return mappaProprieta;
-	}
-
-	@Override
-	public List<String> getRuoliUtentePolisportiva() {
-
-		return new ArrayList<String>();
-	}
-
-	@Override
-	public boolean isEqual(UtentePolisportivaAbstract utenteDaConfrontare) {
-		if (this.getEmail().equals((String) utenteDaConfrontare.getProprieta().get("email"))) {
-			return true;
+		
+		public boolean isSuaQuesta(String email) {
+			return getAnagraficaUtente().getEmail().equals(email);
 		}
-		return false;
+		
+		public String getEmail() {
+			return getAnagraficaUtente().getEmail();
+		}
+		
+		/**
+		 * Metodo che ritorna la password
+		 * @return
+		 */
+		public String getPassword() {
+			return getAnagraficaUtente().getPassword();
+		}
+		
+		public String getNome() {
+			return getAnagraficaUtente().getNome();
+		}
+		
+		
+		public String getCognome() {
+			return getAnagraficaUtente().getCognome();
+		}
+
+		public String getNominativoCompleto() {
+			return getNome() + " " + getCognome();
+		}
 	}
 
-}
+
